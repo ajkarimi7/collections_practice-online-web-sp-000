1ed3d1592bbc6307e3de6455f73619d0584a5ea7
Ali Karimi <ajkarimi7@gmail.com>
2019-09-10 20:45:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,8 +16,7 @@ def sort_array_char_count(array)
   end 
 end 
 
-def swap_elements_from_to(array)
-  array.sort do |a, b|
-    b.index[2] <=> a.index[1] 
-  end 
-end 
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
